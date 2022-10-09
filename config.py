5f5ac3795887bdf2cf4e6779364485efdb3d02ba
Michal Cikatricis <xcikatr@mail.muni.cz>
2022-10-09 14:50:11 +0200

    fix: compatibility with selenium 4.3.x

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 login_url = "https://is.muni.cz/auth"
 student_page = "https://is.muni.cz/auth/student/"
 
-path_to_driver = r"fill me in"
-UCO = "fill me in"
-primary_password = "fill me in"
+UCO = "2116"
+primary_password = "super*secret*password"
+
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 from time import sleep
 
@@ -12,7 +14,9 @@ import config
 
 class EasyPass:
     def __init__(self):
-        self.driver = webdriver.Chrome(config.path_to_driver)
+        options = webdriver.ChromeOptions()
+        options.binary_location = r"C:\Users\micha\AppData\Local\Google\Chrome\Application\chrome.exe"
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.login()
         self.tests_to_do = self.get_list_of_tests_from_file()
         self.list_of_tests_url = self.get_list_of_test_url()
@@ -21,18 +25,21 @@ class EasyPass:
 
     def solve_tests(self):
         for test in self.tests_to_do:
-            link = self.get_test_link(test)
+            try:
+                link = self.get_test_link(test)
+            except AttributeError:
+                continue
             if link is None:
                 continue
             self.driver.get(link)
             self.find(By.XPATH, "//button[contains(text(), 'Spustit nový odpovědník')]").click()
             self.find(By.XPATH, "//button[contains(text(), 'Odevzdat')]").click()
             self.find(By.LINK_TEXT, "Prohlídka").click()
-            answers = self.driver.find_elements_by_xpath("//span[@class='ok']")
+            answers = self.driver.find_elements(By.XPATH, "//span[@class='ok']")
             answers = [answer.text[1:-1].split(',')[0].strip() for answer in answers]
             self.find(By.XPATH, "//a[contains(text(), 'Zpět')]").click()
             self.find(By.XPATH, "//button[contains(text(), 'Spustit nový odpovědník')]").click()
-            cells = self.driver.find_elements_by_xpath("//input[@type='text']")
+            cells = self.driver.find_elements(By.XPATH, "//input[@type='text']")
             for i, cell in enumerate(cells):
                 cell.send_keys(answers[i])
             self.find(By.XPATH, "//button[contains(text(), 'Odevzdat')]").click()
@@ -43,7 +50,7 @@ class EasyPass:
         self.driver.get(config.student_page)
         self.find(By.XPATH, "//a[span/strong[contains(text(), 'ONLINE_A')]]").click()
         self.find(By.XPATH, ".//div[@class='row student_row_b']/div/a")
-        link = self.driver.find_elements_by_xpath(".//div[@class='row student_row_b']/div/a")[-2].get_attribute("href")
+        link = self.driver.find_elements(By.XPATH, ".//div[@class='row student_row_b']/div/a")[-2].get_attribute("href")
         return link
 
     def open_list_of_tests(self):
@@ -52,10 +59,10 @@ class EasyPass:
     def login(self):
         self.driver.get(config.login_url)
 
-        elem = self.driver.find_element_by_name("credential_0")
+        elem = self.driver.find_element(By.NAME, "credential_0")
         elem.send_keys(config.UCO)
 
-        elem = self.driver.find_element_by_name("credential_1")
+        elem = self.driver.find_element(By.NAME, "credential_1")
         elem.send_keys(config.primary_password)
         elem.send_keys(Keys.RETURN)
 
@@ -73,15 +80,16 @@ class EasyPass:
             return [line.strip() for line in file.readlines()]
 
     def get_test_link(self, test):
-        element = self.find(By.XPATH, f".//tr[td/a[contains(text(), '{test}')]]")
+        element = self.find(By.XPATH, f"//*[@id=\"vyber_odpovedniku_body\"]/div/div/div/div[2]/div/table/tbody//tr["
+                                      f"td/a[contains(text(), '{test}')]]")
         try:
-            points = element.find_element_by_xpath("td/div/div/span[@class='pozn_blok']")
+            points = element.find_element(By.XPATH, "td/div/div/span[@class='pozn_blok']")
             if points.text != "*0":
                 return
-            test_link = points.find_element_by_xpath("../../../../td[@class='uzel']/a").get_attribute("href")
+            test_link = points.find_element(By.XPATH, "../../../../td[@class='uzel']/a").get_attribute("href")
             return test_link
         except NoSuchElementException:
-            test_link = element.find_element_by_xpath("td[@class='uzel']/a").get_attribute("href")
+            test_link = element.find_element(By.XPATH, "td[@class='uzel']/a").get_attribute("href")
             return test_link
 
 
